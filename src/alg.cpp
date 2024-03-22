339fd29f52b1f8da30a371822e5825e59e03bf2d
Сергей Букарин <SerBuka2006@yandex.ru>
2024-03-22 18:27:45 +0300

    ADS 3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,32 +1,28 @@
-// Copyright 2021 NNTU-CS
-
-int cbinsearch(int *arr, int size, int value) {
+int cbinsearch(int* arr, int size, int value) {
     int count = 0;
-    int left = 0;
-    int right = size - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid] == value) {
-            count++;
-            int i = mid - 1;
-            while (i >= 0 && arr[i] == value) {
-                count++;
-                i--;
-            }
-            i = mid + 1;
-            while (i < size && arr[i] == value) {
-                count++;
-                i++;
-            }
-            return count;
+    int l = *arr;
+    int r = size - 1;
+    while (l < r) {
+        int mid = (l + r) / 2;
+        if (r - l == 1) {
+            return 0;
         }
-        else if (arr[mid] < value) {
-            left = mid + 1;
+        if (value > arr[mid]) {
+            l = mid;
+        }
+        else if (value < arr[mid]) {
+            r = mid;
         }
         else {
-            right = mid - 1;
+            while (arr[mid] == value) {
+                mid--;
+            }
+            mid++;
+            while (arr[mid] == value) {
+                mid++;
+                count++;
+            }
+            break;
         }
     }
     return count;
