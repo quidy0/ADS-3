87c43d40b3a67fe00a874df9c8506d85d53293a0
quidy0 <160316930+quidy0@users.noreply.github.com>
2024-03-31 13:48:44 +0300

    155.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int count = 0;
+    int l = *arr;
+    int r = size - 1;
+    while (l < r) {
+        int mid = (l + r) / 2;
+        if (r - l == 1) {
+            return 0;
+        }
+        if (value > arr[mid]) {
+            l = mid;
+        } else if (value < arr[mid]) {
+            r = mid;
+        } else {
+            while (arr[mid] == value) {
+                mid--;
+            }
+            mid++;
+            while (arr[mid] == value) {
+                mid++;
+                count++;
+            }
+            break;
+        }
+    }
+    return count;
 }
